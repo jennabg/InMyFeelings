e13850fbd04e85d79687467596212cd17e42fc6e
Jenna Greenberg <jenna.b.greenberg@gmail.com>
2019-04-25 13:30:04 -0400

    ts

--- about.php
+++ about.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
   <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
   <meta charset="utf-8"/>
@@ -17,23 +15,18 @@
   </nav>
   <main id="main">
     <div id="background">
-      <h1> What are you feeling? </h1>
-      <input type="text" id="feeling"/>
-      <button id="submit" type="button"> Find my feeling! </button>
-      <div id="results">
-        <div id="emotionresult"></div>
-        <div id="bookresult"></div>
-        <div id="movieresult"></div>
-      </div>
+      <h1> About In My Feelings </h1>
+      <p> This website was made as a school project for Humber's Web Development program.
+        The site can be used to help users select a book or movie based on how they are feeling. The project was to
+        create a site that used at least two APIs. This project uses the Google Books API, the Movie Database API
+        and The Noun Project API.
+        Thank you and enjoy the website! </p>
     </div>
   </main>
   <footer id="footer">
     <p>Copyright Jenna Greenberg 2019 </p>
   </footer>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.4.0.min.js" integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous"></script>
-  <script src="script/script.js"></script>
 </body>
 
 </html>
